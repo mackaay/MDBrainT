cd7c8d4447842caeb1ea5410e92994dfff82ade8
C.MA <11036337+mackaay@users.noreply.github.com>
2025-01-19 22:11:44 +1100

    Add files via upload

--- R/MDBrainT.R
+++ R/MDBrainT.R
@@ -15,18 +15,18 @@
 #'   sig_matrix <- system.file("extdata", "brain_sig_matrix.txt", package = "MDBrainT")
 #'   mixture_file <- system.file("extdata", "example_brain.txt", package = "MDBrainT")
 #'   celltype_anno <- system.file("extdata", "celltype_anno.txt", package = "MDBrainT")
-#'   results <- MDBrainT(sig_matrix, mixture_file, cell_annotation = celltype_anno)
+#'   results <- TMEdeconvolute(sig_matrix, mixture_file, cell_annotation = celltype_anno)
 #'   ## example 2
 #'   data(brain_sig_matrix)
 #'   data(example_brain)
 #'   data(celltype_anno)
-#'   results <- MDBrainT(sig_matrix = brain_sig_matrix, mixture_file = example_brain, cell_annotation = celltype_anno)
+#'   results <- TMEdeconvolute(sig_matrix = brain_sig_matrix, mixture_file = example_brain, cell_annotation = celltype_anno)
 #' }
 
 require("nnls")
 
 
-MDBrainT <- function(sig_matrix, mixture_file, cell_annotation){
+TMEdeconvolute <- function(sig_matrix, mixture_file, cell_annotation){
   
   #read in data
   if (is.character(sig_matrix)) {
